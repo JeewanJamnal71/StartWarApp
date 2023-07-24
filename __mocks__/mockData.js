ab4d76a1814c75bbefd58a8f3b1c44f80dcdffb4
Jeewan <jeewan.jamnal@mindbowser.com>
2023-07-24 10:25:52 +0530

    code update

---
+++ __mocks__/mockData.js
@@ -0,0 +1,22 @@
+export const personMockData = {
+    name: 'John Doe',
+    height: 1.72,
+    mass: '77',
+    createdDate: '2023-07-19T12:34:56.000Z',
+    films: ['https://swapi.dev/api/films/1/', 'https://swapi.dev/api/films/2/'],
+    dob: '19BBY',
+    picture: 'https://example.com/john-doe.jpg',
+    cardColor: '#111',
+    speciesId: 1,
+    filmsLength: 2,
+    homeworld: 'https://swapi.dev/api/planets/28/',
+    image: 'https://picsum.photos/300/400',
+    gender: 'male'
+};
+
+export const homeworldMockData = {
+    name: 'Tatooine',
+    terrain: 'Desert',
+    climate: 'Arid',
+    residents: ['https://swapi.dev/api/people/1/', 'https://swapi.dev/api/people/2/'],
+};
--- __tests__/Integration.test.js
+++ __tests__/Integration.test.js
@@ -1,67 +1,33 @@
 import 'react-native';
 import React from 'react';
 import {it} from '@jest/globals';
-import { render, fireEvent, act } from '@testing-library/react-native';
+import { render, fireEvent } from '@testing-library/react-native';
 import CardComponent from '../src/components/CardComponent/CardComponent';
 import ModalComponent from '../src/components/ModalComponent/ModalComponent';
+import { personMockData, homeworldMockData } from '../__mocks__/mockData';
 
 jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'))
 
 describe('Integration Test', () => {
   it('should open the modal when a card is pressed', () => {
-    // dummy data
-    const PersonMockData = {
-      name: 'John Doe',
-      height: 1.72,
-      mass: '77',
-      createdDate: '2023-07-19T12:34:56.000Z',
-      films: ['https://swapi.dev/api/films/1/', 'https://swapi.dev/api/films/2/'],
-      dob: '19BBY',
-      picture: 'https://example.com/john-doe.jpg',
-      cardColor: '#111',
-      speciesId: 1,
-      filmsLength: 2,
-      homeworld: 'https://swapi.dev/api/planets/28/',
-      gender: 'male'
-    };
+   
     const setModalVisible = jest.fn();
     const { getByTestId } = render(
-      <CardComponent item={PersonMockData} onSelectItem={setModalVisible} />
+      <CardComponent item={personMockData} onSelectItem={setModalVisible} />
     );
     const card = getByTestId('card-component');
     fireEvent.press(card);
     expect(setModalVisible).toHaveBeenCalled();
+    
   });
 
   it('should display the correct person information in the modal', async() => {
-    
-    // dummy data
-    const PersonMockData = {
-      name: 'John Doe',
-      height: 1.72,
-      mass: '77',
-      createdDate: '2023-07-19T12:34:56.000Z',
-      films: ['https://swapi.dev/api/films/1/', 'https://swapi.dev/api/films/2/'],
-      dob: '19BBY',
-      image: 'https://picsum.photos/300/400',
-      filmsLength: 2,
-      gender: 'male'
-    };
-
-    const HomeworldMockData = {
-      name: 'Tatooine',
-      terrain: 'Desert',
-      climate: 'Arid',
-      residents: ['https://swapi.dev/api/people/1/', 'https://swapi.dev/api/people/2/'],
-    };
-
-    const modalVisible = true;
-
+  
     const { getByText, getByTestId } = render(
       <ModalComponent
-        data={PersonMockData}
-        homeData={HomeworldMockData}
-        modalVisible={modalVisible}
+        data={personMockData}
+        homeData={homeworldMockData}
+        modalVisible={true}
         onClose={() => {}}
         loading={false}
         error={null}
@@ -103,9 +69,7 @@ describe('Integration Test', () => {
     expect(getByText('19-07-2023')).toBeTruthy();
     
     const homelandOption = getByText('Homeland');
-    act(() => {
-      fireEvent.press(homelandOption);
-    })
+    fireEvent.press(homelandOption);
 
     // HomeLand Info
 
@@ -120,6 +84,10 @@ describe('Integration Test', () => {
     //Climate
     expect(getByText('Climate')).toBeTruthy();
     expect(getByText('Arid')).toBeTruthy();
+
+    //Number of residents
+    expect(getByText('Residents')).toBeTruthy();
+    expect(getByText('2')).toBeTruthy();
 
   });
 
